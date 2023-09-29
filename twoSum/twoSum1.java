c5c44c6315f3946419e5a01e4143c55943784470
Huy Tran <baohuy82.th@gmail.com>
2023-09-29 01:45:39 -0700

    commit

---
+++ twoSum/twoSum1.java
@@ -0,0 +1,11 @@
+class twoSum1 {
+    public int[] twoSum(int[] nums, int target) {
+        nums[] = [2,7,11,15];
+        int target = 9;
+        n = nums.length;
+
+        for (i = 0, i < n, i++)
+            for (j = i+1, j < n, j++ )
+            if (nums[i] + nums[j] == target)
+    }
+}
