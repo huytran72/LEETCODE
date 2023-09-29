b6c543e83519753c5b31362a324b62b0af44f4ce
Huy Tran <baohuy82.th@gmail.com>
2023-09-29 02:48:10 -0700

    commit

---
+++ twoSum2.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class twoSum2 {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> arrMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            arrMap.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int newtarget = target - nums[i];
+            if(arrMap.containsKey(newtarget) && arrMap.get(newtarget) != i) {
+                return new int[] {i, newtarget};
+            }
+        }
+        return new int[] {};
+    }
+}
