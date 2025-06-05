274db0bf79c43c186c11bb40d563bf5b7b2d8b4b
Huy Tran <baohuy82.th@gmail.com>
2025-06-04 22:57:21 -0700

    Update solution_1.java

--- _1295_Even_Number_Digits/solution_1.java
+++ _1295_Even_Number_Digits/solution_1.java
@@ -2,6 +2,31 @@ package _1295_Even_Number_Digits;
 
 
 public class solution_1 {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+
+        for (int a: nums) {
+            if(numsofDigits(a) % 2 == 0) {
+                count++;
+            }
+        }
+
+
+        
+
+        return count;
+    }
+
+
+    private int numsofDigits(int a) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'numsofDigits'");
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
     
 }
 
