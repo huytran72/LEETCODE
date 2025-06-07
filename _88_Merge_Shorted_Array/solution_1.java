a705e1031550d137aece4727fb7fc73ed7d6b04d
Huy Tran <baohuy82.th@gmail.com>
2025-06-07 00:33:57 -0700

    Create solution_1.java

---
+++ _88_Merge_Shorted_Array/solution_1.java
@@ -0,0 +1,40 @@
+// package _88_Merge_Shorted_Array;
+
+public class solution_1 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] merged = new int[m+n];
+        int i = 0; int j = 0; int k = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+
+        while (j < n) {
+            merged[k++] = nums2[j++];
+        }
+
+        while ( i< m) {
+            merged[k++] = nums1[i++];
+        }
+
+        for (int x = 0; x < m + n; i++) {
+            merged[x] = nums1[x];
+        }
+    }
+
+    //Test case
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        solution_1 solution = new solution_1();
+        solution.merge(nums1, m, nums2, n);
+        
+    }
+}
