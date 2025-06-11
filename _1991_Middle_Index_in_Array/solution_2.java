ef243c45b4c2c9085e542ff6907614ff7bd72342
Huy Tran <baohuy82.th@gmail.com>
2025-06-11 13:21:37 -0700

    Update solution_2.java

--- _1991_Middle_Index_in_Array/solution_2.java
+++ _1991_Middle_Index_in_Array/solution_2.java
@@ -4,7 +4,7 @@
 // 1. Calculate the total sum of the array.
 // 2. Loop the array from left to right
 // 2.1. leftSum is the sum of elements from 0 to i - 1
-// 2.2. rightSum is totalSum - leftSum - nums[i]
+// 2.2. rightSum is sum - leftSum - nums[i]
 // 3. If leftSum equals rightSum, return i.
 // 4. If no such index is found, return -1.
 
