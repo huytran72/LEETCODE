ac1b16217e1a6ffdef75d6fcc8528cb3485f6bcf
Huy Tran <baohuy82.th@gmail.com>
2025-06-10 22:52:56 -0700

    Update solution_2.java

--- _1991_Middle_Index_in_Array/solution_2.java
+++ _1991_Middle_Index_in_Array/solution_2.java
@@ -3,6 +3,7 @@
 // Idea:
 // 1. Calculate the total sum of the array.
 // 2. Loop the array from left to right
+// 2.1. leftSum is the sum of elements from 0 to i - 1
 
 public class solution_2 {
     public static int findMiddleIndex(int[] nums) {
