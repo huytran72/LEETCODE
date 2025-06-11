598ca881bdb8f9411d1863bb440ba06d176cabe4
Huy Tran <baohuy82.th@gmail.com>
2025-06-10 22:51:44 -0700

    Create solution_2.java

---
+++ _1991_Middle_Index_in_Array/solution_2.java
@@ -0,0 +1,10 @@
+// package _1991_Middle_Index_in_Array;
+
+// Idea:
+// 1. Calculate the total sum of the array.
+
+public class solution_2 {
+    public static int findMiddleIndex(int[] nums) {
+
+    };
+}
