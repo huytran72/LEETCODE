68bf86c5b3d0a2efa51e29952a262c54c8e79cf9
Huy Tran <baohuy82.th@gmail.com>
2025-06-11 13:24:36 -0700

    Update solution_2.java

--- _1991_Middle_Index_in_Array/solution_2.java
+++ _1991_Middle_Index_in_Array/solution_2.java
@@ -35,8 +35,11 @@ public class solution_2 {
         int[] nums = {2, 3, -1, 8, 4};  // Output: 3
         int[] nums2 = {1, -1, 4};          // Output: 2
         int[] nums3 = {2, 5};               // Output: -1
+        int[] nums4 = {1,3,4,7,8,3,4,-2,6}; // Output: 3
+
         System.out.println(findMiddleIndex(nums)); 
         System.out.println(findMiddleIndex(nums2));
         System.out.println(findMiddleIndex(nums3));
+        System.out.println(findMiddleIndex(nums4));
     }
 }
