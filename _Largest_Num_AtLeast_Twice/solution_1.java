e22d9a2b74aad9b1c74c9b7d002b822ce6acee5b
Huy Tran <baohuy82.th@gmail.com>
2025-06-12 23:35:48 -0700

    Create solution_1.java

---
+++ _Largest_Num_AtLeast_Twice/solution_1.java
@@ -0,0 +1,11 @@
+// package _Largest_Num_AtLeast_Twice;
+
+// You are given an integer array nums where the largest integer is unique.
+// Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+
+
+
+
+public class solution_1 {
+    
+}
