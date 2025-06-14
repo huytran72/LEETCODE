405ecb31eb5e58d794437cd949ec5c9f9ba63fd5
Huy Tran <baohuy82.th@gmail.com>
2025-06-13 23:06:33 -0700

    Update solution_2.java

--- _Largest_Num_AtLeast_Twice/solution_2.java
+++ _Largest_Num_AtLeast_Twice/solution_2.java
@@ -2,6 +2,9 @@
 
 // Solution 2: Using a single pass to find the largest number and check the condition simultaneously
 
+// Idea:
+// 1. Loop and find 1st max and 2nd max
+
 public class solution_2 {
     
 }
