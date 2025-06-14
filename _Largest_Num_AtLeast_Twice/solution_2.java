52736767c0b8a632ed87186a32f4cc4323a832a0
Huy Tran <baohuy82.th@gmail.com>
2025-06-13 23:05:47 -0700

    Update solution_2.java

--- _Largest_Num_AtLeast_Twice/solution_2.java
+++ _Largest_Num_AtLeast_Twice/solution_2.java
@@ -1,4 +1,6 @@
-package _Largest_Num_AtLeast_Twice;
+// package _Largest_Num_AtLeast_Twice;
+
+// Solution 2: Using a single pass to find the largest number and check the condition simultaneously
 
 public class solution_2 {
     
