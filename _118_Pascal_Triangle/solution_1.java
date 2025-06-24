e3f04491618a4c8db4187f6f80dedc1dfbf9172c
Huy Tran <baohuy82.th@gmail.com>
2025-06-23 23:49:59 -0700

    Update solution_1.java

--- _118_Pascal_Triangle/solution_1.java
+++ _118_Pascal_Triangle/solution_1.java
@@ -6,11 +6,23 @@ import java.util.List;
 public class solution_1 {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> pascalTriangle = new ArrayList<>();
+
+        // Handle edge cases
+        if(numRows <= 0) {
+            return pascalTriangle;
+        } else if(numRows == 1) {
+            pascalTriangle.add(List.of(1));
+            return pascalTriangle; 
+        } else if(numRows == 2) {
+            pascalTriangle.add(List.of(1));
+            pascalTriangle.add(List.of(1, 1));
+            return pascalTriangle;
+        }
     }
 
 
 
-    
+
     public static void main(String[] args) {
         
     }
