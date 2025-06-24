a4cc84966daf621cf4e674b3483a1523828d1102
Huy Tran <baohuy82.th@gmail.com>
2025-06-23 23:49:44 -0700

    Update solution_1.java

--- _118_Pascal_Triangle/solution_1.java
+++ _118_Pascal_Triangle/solution_1.java
@@ -1,5 +1,17 @@
 // package _118_Pascal_Triangle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class solution_1 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> pascalTriangle = new ArrayList<>();
+    }
+
+
+
     
+    public static void main(String[] args) {
+        
+    }
 }
