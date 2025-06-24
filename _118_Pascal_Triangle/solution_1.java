b15c6aeabbc0bb9c22777a2403f7141eef0b706d
Huy Tran <baohuy82.th@gmail.com>
2025-06-23 23:50:10 -0700

    Update solution_1.java

--- _118_Pascal_Triangle/solution_1.java
+++ _118_Pascal_Triangle/solution_1.java
@@ -18,6 +18,27 @@ public class solution_1 {
             pascalTriangle.add(List.of(1, 1));
             return pascalTriangle;
         }
+
+        // Base case (numRows >= 3)
+        for(int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+
+            // First element is 1
+            row.add(1);
+
+            // Calculate values in the middle
+            for(int j = 1; j < i; j++) {
+                int value = pascalTriangle.get(i-1).get(j-1) + pascalTriangle.get(i-1).get(j);
+                row.add(value);
+            }
+
+            // The last element is 1
+            if(i > 0) {
+                row.add(1);
+            }
+
+            // Add the new row to the list
+            pascalTriangle.add(row);
     }
 
 
