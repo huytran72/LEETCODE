8551b0d488ddf4b2538249d8265ed7895db55bd3
Huy Tran <baohuy82.th@gmail.com>
2025-07-15 00:19:43 -0700

    Update Contains_Duplicate.java

--- _217_/Contains_Duplicate.java
+++ _217_/Contains_Duplicate.java
@@ -4,4 +4,45 @@ public class Contains_Duplicate {
     
 }
 
+/*
+*** Solution 1: Brute Force
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] == nums[j]) return true;
+            }
+        }
+        return false;
 
+--------------------------------------
+*** Solution 2: Hashmap
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    for(int num: nums) {
+        map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+
+    for(int i: map.values()) {
+        if(i != 1) return true;
+    }
+    return false;
+
+- TC: O(n)
+- SC: O(n)
+
+
+HashMap:
+1: 2
+2: 1
+3: 1
+
+--------------------------------------
+*** Solution 3: Set
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) return true;
+        }
+        return false;
+- TC: O(n)
+- SC: O(n)
+
+*/
